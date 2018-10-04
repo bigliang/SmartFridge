47722cf590828398f9145c8a1c90bcf5442c9460
bigliang <shi.1128@osu.edu>
2018-10-04 10:55:18 -0500

    Initial Commit

---
+++ SmartCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartCamera
+//
+//  Created by  shihongliang on 10/4/18.
+//  Copyright © 2018  shihongliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmartCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartCamera
+//
+//  Created by  shihongliang on 10/4/18.
+//  Copyright © 2018  shihongliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
