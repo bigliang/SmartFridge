2e0cc0035f902d10cbef11c55628d4e75fb28872
bigliang <shi.1128@osu.edu>
2018-11-21 16:41:09 -0500

    aaa

---
+++ SmartCameraTests/SmartCameraTests.m
@@ -0,0 +1,37 @@
+//
+//  SmartCameraTests.m
+//  SmartCameraTests
+//
+//  Created by  shihongliang on 11/19/18.
+//  Copyright © 2018  shihongliang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SmartCameraTests : XCTestCase
+
+@end
+
+@implementation SmartCameraTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
